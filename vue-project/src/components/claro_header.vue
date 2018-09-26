94b182b35a8deeffbe0a74bd10b4e071a9e5fc54
KelvinArnold <kflores.0025@outlook.com>
2018-09-26 20:44:38 -0300

    Vue Version for Claro Challenge

---
+++ vue-project/src/components/claro_header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="clr-header">
+    <div class="container-fluid">
+      <div class="row d-block bgc-light pt-2 pb-3 pl-3 pr-3">
+        <p class="clr-back">
+          <router-link tag="span" to="/" class="tc-black">
+          <arrow-icon v-if="this.$route.name != 'home'"></arrow-icon> Home</router-link>
+        </p>
+        <h4 class="title tc-primary font-weight-bold">Claro</h4>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+import ArrowIcon from './../assets/images/arrow-icon.svg';
+
+export default {
+  name: 'claro-header',
+  components: {
+    ArrowIcon
+  }
+}
+</script>
+
+<style scoped>
+  
+</style>
---
+++ vue-project/src/components/claro_icons.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="claro-icon">
+    <celular-icon v-if="icon === 'celular'"></celular-icon>
+    <fixo-icon v-else-if="icon === 'fixo'"></fixo-icon>
+    <internet-icon v-else-if="icon === 'internet'"></internet-icon>
+    <tv-icon v-else></tv-icon>
+  </div>
+</template>
+
+<script>
+import CelularIcon from './../assets/images/celular.svg';
+import FixoIcon from './../assets/images/fixo.svg';
+import InternetIcon from './../assets/images/internet.svg';
+import TvIcon from './../assets/images/tv.svg';
+
+export default {
+  name: 'claro-icons',
+  data() {
+    return {}
+  },
+  props: {
+    icon: {
+      type: String,
+      default: ''
+    }
+  },
+  components: {
+    CelularIcon,
+    FixoIcon,
+    InternetIcon,
+    TvIcon
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .claro-icon {
+    display: inline-block;
+    width: 40px;
+    height: 40px;
+    svg {
+      display: inline-block;
+      transform: scale(.3) translateX(-25%) translateY(-25%);
+      position: absolute;
+      left: 0;
+      top: 0;
+    }
+  }
+</style>
